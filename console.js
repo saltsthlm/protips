427faa3cb8596652ed3871106d0a1fe10e38d9b8
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-03-18 09:46:52 +0100

    Small updates to console.js

--- console.js
+++ console.js
@@ -3,6 +3,10 @@ const name = 'Marcus';
 // console.log(`Name of ${name}:`, name);
 console.log({ name })
 
+
+
+console.markTimeline('#############################################')
+
 // const longRunning = (cb) => {
 //   setTimeout(() => {
 //     cb()
@@ -14,25 +18,25 @@ console.log({ name })
 //   console.timeEnd('LongRunningTimer')
 // })
 
-// const people = [{
-//   name: 'Marcus',
-//   height: 196,
-//   age: 'Gandalf-ish'
-// },
-// {
-//   name: 'Lars',
-//   height: 186,
-//   age: 'In his prime'
-// },
-// {
-//   name: 'Mies',
-//   height: 176,
-//   age: 'Drying behind ears'
-// }]
+const people = [{
+  name: 'Marcus',
+  height: 196,
+  age: 'Gandalf-ish'
+},
+{
+  name: 'Lars',
+  height: 186,
+  age: 'In his prime'
+},
+{
+  name: 'Mies',
+  height: 176,
+  age: 'Drying behind ears'
+}]
 
-// console.log(people)
-// console.table(people)
+console.log(people)
+console.table(people)
 
-// const { name, height, age } = person[0]
+// const { name, height, age } = people[0]
 // console.log(name, height, age)
 // console.log({ name, height, age })
